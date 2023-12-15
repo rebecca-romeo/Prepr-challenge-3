9b03950bd4e51f69464ac77b64c78e357dd84ecc
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 12:13:07 -0500

    create Upcoming card  component

---
+++ src/components/cards/Upcoming.js
@@ -0,0 +1,49 @@
+const Upcoming = () => {
+  return (
+      <div className="cards upcomingContainer">
+      <h3>Upcoming</h3>
+
+      <div className="eventContainer">
+        <i class="bi bi-calendar3"></i>
+        <div>
+          <p className="bold">IFC Live Challenge - Transportation</p>
+          <p>Due October 6 at 12:00am</p>
+        </div>
+      </div>
+
+      <div className="eventContainer">
+        <i class="bi bi-calendar3"></i>
+        <div>
+          <p className="bold">IFC Live Challenge - Transportation</p>
+          <p>Due October 6 at 12:00am</p>
+        </div>
+      </div>
+
+      <div className="eventContainer">
+        <i class="bi bi-calendar3"></i>
+        <div>
+          <p className="bold">IFC Live Challenge - Transportation</p>
+          <p>Due October 6 at 12:00am</p>
+        </div>
+      </div>
+      <div className="eventContainer">
+        <i class="bi bi-calendar3"></i>
+        <div>
+          <p className="bold">IFC Live Challenge - Transportation</p>
+          <p>Due October 6 at 12:00am</p>
+        </div>
+      </div>
+
+      <div className="eventContainer">
+        <i class="bi bi-calendar3"></i>
+        <div>
+          <p className="bold">IFC Live Challenge - Transportation</p>
+          <p>Due October 6 at 12:00am</p>
+        </div>
+      </div>
+
+      </div>
+  )
+}
+
+export default Upcoming;
