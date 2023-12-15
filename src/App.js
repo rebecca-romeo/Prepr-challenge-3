89eb27c267a2daf45e7da5bf13361cb446a75b40
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 10:59:57 -0500

    add Footer component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+      <Footer />
     </div>
   );
 }
