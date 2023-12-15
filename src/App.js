b09a2b21f3641211f4a4f6ca03a10c76763f2c31
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 10:52:42 -0500

    add NavBar to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
 
     </div>
   );
